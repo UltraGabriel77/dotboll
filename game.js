06b6b48ea5a1ac3cb0ceeba620646ee8ad69385e
UltraGabriel77 <ghsilha@gmail.com>
2020-09-16 12:47:33 +0000

    Server connection and Player object

---
+++ game.js
@@ -0,0 +1,22 @@
+var Phaser = require("phaser");
+var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+
+function init() {
+    
+}
+
+module.exports = init;
--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+const { Player } = require("./Player");
+
 var app = require("express")();
 var http = require("http").createServer(app);
+var io = require('socket.io')(http);
+
+var jogadores = {};
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, ()=>{
-    console.log('hosting on *:3000')
+io.on('connection',(socket)=>{
+    var player = newPlayer(100,100,socket.id)   //Cria um novo jogador
+    Object.values(jogadores).forEach(e => {     //Coloca os outros jogadoresna lista de times
+        if (e.time!=0){
+            socket.emit('time', e);
+        }
+    });                                     
+    console.log("User conected: " + player.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected: '+ socket.id);
+        io.emit('disconnect',jogadores[socket.id])
+    });
+    socket.on('time',(e)=>{
+        console.log(e);
+        jogadores[e[1]].time = e[0];
+        io.emit('reload',jogadores[e[1]]);
+    });
+    socket.on('name',(e)=>{
+        console.log(e);
+        jogadores[e[0]].name = e[1];
+        io.emit('reload',jogadores[e[1]]);
+    });
+
 });
+
+http.listen(3000, ()=>{
+    console.log('hosting on *:3000');
+});
+
+function newPlayer(x,y,id) {
+    var player = new Player(x,y,id);
+    jogadores[id] = player;
+    return player;
+}
+

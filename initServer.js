d454a858320650d1c507dfdbb2c09b2e29889472
Gabriel Henrique Silva <ghsilha@gmail.com>
2020-09-27 14:32:56 -0300

    Ball and window

--- initServer.js
+++ initServer.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 /**
  * Iniciar o server e pegar os eventos
